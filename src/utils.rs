1eddfcbacc03364e3af1b82c562acb5cc990f39b
Felix Braeunling <gronner@mailbox.org>
2025-12-01 02:52:25 +0100

    Add regex utility

---
+++ src/utils.rs
@@ -0,0 +1,7 @@
+#[allow(unused)]
+macro_rules! regex {
+    ($re:literal $(,)?) => {{
+        static RE: std::sync::OnceLock<regex::Regex> = std::sync::OnceLock::new();
+        RE.get_or_init(|| regex::Regex::new($re).unwrap())
+    }};
+}
